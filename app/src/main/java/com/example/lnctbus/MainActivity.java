372ad251b524acc9d05b7d98d0320e9536e085ee
Prateek <prateekj297@gmail.com>
2019-06-08 09:21:52 +0530

    Fix project and re add

---
+++ app/src/main/java/com/example/lnctbus/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.lnctbus;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.editText3);
+    }
+
+    public void Submit(View view) {
+        String destination = editText.getText().toString();
+
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(destination, "text", "text");
+    }
+}
